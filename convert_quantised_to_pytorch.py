b0757b2e152f7e86d2754a58031507efd7595e74
Chris Bao <89361988+sscg13@users.noreply.github.com>
2026-01-24 15:04:55 -0800

    Update convert_quantised_to_pytorch.py

--- convert_quantised_to_pytorch.py
+++ convert_quantised_to_pytorch.py
@@ -1,7 +1,7 @@
 import struct
 import argparse
 
-FEATURE_SET_HASH = 0x7f234cb8
+FEATURE_SET_HASH = 0x8f234cb8
 VERSION = 0x7AF32F20
 DEFAULT_DESCRIPTION = "Network trained with the https://github.com/official-stockfish/nnue-pytorch trainer."
 
@@ -17,29 +17,29 @@ def encode_leb_128_array(arr):
             res.append(byte | 0x80)
     return res
 
-# def fc_hash(L1, L2, L3, num_ls_buckets=8):
-#     # InputSlice hash
-#     prev_hash = 0xEC42E90D
-#     prev_hash ^= (L1 * 2)
+def fc_hash(L1, L2, L3, num_ls_buckets=8):
+    # InputSlice hash
+    prev_hash = 0xEC42E90D
+    prev_hash ^= (L1 * 2)
 
-#     # Fully connected layers
-#     layers_info = [
-#         (L2 + 1, True), 
-#         (L3, True),      
-#         (1, False)      
-#     ]
+    # Fully connected layers
+    layers_info = [
+        (L2 + 1, True), 
+        (L3, True),      
+        (1, False)      
+    ]
     
-#     for out_features, has_relu in layers_info:
-#         layer_hash = 0xCC03DAE4
-#         layer_hash += out_features // num_ls_buckets
-#         layer_hash ^= prev_hash >> 1
-#         layer_hash ^= (prev_hash << 31) & 0xFFFFFFFF
-#         if out_features // num_ls_buckets != 1 and has_relu:
-#             # Clipped ReLU hash
-#             layer_hash = (layer_hash + 0x538D24C7) & 0xFFFFFFFF
-#         prev_hash = layer_hash
+    for out_features, has_relu in layers_info:
+        layer_hash = 0xCC03DAE4
+        layer_hash += out_features // num_ls_buckets
+        layer_hash ^= prev_hash >> 1
+        layer_hash ^= (prev_hash << 31) & 0xFFFFFFFF
+        if out_features // num_ls_buckets != 1 and has_relu:
+            # Clipped ReLU hash
+            layer_hash = (layer_hash + 0x538D24C7) & 0xFFFFFFFF
+        prev_hash = layer_hash
     
-#     return prev_hash
+    return prev_hash
 
 def write_header(outfile, L1, fc_hash_val, description=DEFAULT_DESCRIPTION):
     # Write version
@@ -47,7 +47,7 @@ def write_header(outfile, L1, fc_hash_val, description=DEFAULT_DESCRIPTION):
     
     # Write network hash
     # halfkp_hash = fc_hash_val ^ FEATURE_SET_HASH ^ (L1 * 2)
-    halfkp_hash = 0x1C1020F2
+    halfkp_hash = 0xEC103C92
     outfile.write(struct.pack('<I', halfkp_hash))
     
     # Write description
@@ -60,35 +60,38 @@ def write_leb_128_array(outfile, arr):
     outfile.write(struct.pack('<I', len(buf)))
     outfile.write(bytes(buf))
 
-def read_binary_file(filename, L1, L2, L3, inputs, num_buckets=8):
+def read_binary_file(filename, L1, L2, L3, num_buckets=8):
     with open(filename, 'rb') as f:
         data = {}
         
-        # Read l0b (16-bit) - 3072 values
-        data['l0b'] = list(struct.unpack('<' + 'h' * 3072, f.read(3072 * 2)))
+        # Read l0b (16-bit) - L1 values
+        data['l0b'] = list(struct.unpack('<' + 'h' * L1, f.read(L1 * 2)))
         
-        # Read l0w (16-bit) - 69206016 values (inputs * L1)
-        data['l0w'] = list(struct.unpack('<' + 'h' * (inputs * L1), f.read(inputs * L1 * 2)))
+        # Read l0p (16-bit) - (22528 * L1) values 
+        data['l0p'] = list(struct.unpack('<' + 'h' * (22528 * L1), f.read(22528 * L1 * 2)))
         
-        # Read pst (32-bit) - 720896 values (inputs * 8)
-        data['pst'] = list(struct.unpack('<' + 'i' * (inputs * num_buckets), f.read(inputs * num_buckets * 4)))
+        # Read l0t (16-bit) - (79856 * L1) values
+        data['l0t'] = list(struct.unpack('<' + 'h' * (79856 * L1), f.read(79856 * L1 * 2)))
         
-        # Read l1b (32-bit) - 128 values ((L2 + 1) * 8)
+        # Read pst (32-bit) - (102384 * num_buckets) values 
+        data['pst'] = list(struct.unpack('<' + 'i' * (102384 * num_buckets), f.read(102384 * num_buckets * 4)))
+        
+        # Read l1b (32-bit) - ((L2 + 1) * num_buckets) values 
         data['l1b'] = list(struct.unpack('<' + 'i' * ((L2 + 1) * num_buckets), f.read((L2 + 1) * num_buckets * 4)))
         
-        # Read l1w (8-bit) - 393216 values (L1 * (L2 + 1) * 8)
+        # Read l1w (8-bit) - (L1 * (L2 + 1) * num_buckets) values 
         data['l1w'] = list(struct.unpack('<' + 'b' * (L1 * (L2 + 1) * num_buckets), f.read(L1 * (L2 + 1) * num_buckets)))
         
-        # Read l2b (32-bit) - 256 values (L3 * 8)
+        # Read l2b (32-bit) - (L3 * num_buckets) values 
         data['l2b'] = list(struct.unpack('<' + 'i' * (L3 * num_buckets), f.read(L3 * num_buckets * 4)))
         
-        # Read l2w (8-bit) - 7680 values (L2 * 2 * L3 * 8)
+        # Read l2w (8-bit) - (L2 * 2 * L3 * num_buckets) values 
         data['l2w'] = list(struct.unpack('<' + 'b' * (L2 * 2 * L3 * num_buckets), f.read(L2 * 2 * L3 * num_buckets)))
         
-        # Read l3b (32-bit) - 8 values (8)
+        # Read l3b (32-bit) - num_buckets values 
         data['l3b'] = list(struct.unpack('<' + 'i' * num_buckets, f.read(num_buckets * 4)))
         
-        # Read l3w (8-bit) - 256 values (L3 * 8)
+        # Read l3w (8-bit) - (L3 * num_buckets) values 
         data['l3w'] = list(struct.unpack('<' + 'b' * (L3 * num_buckets), f.read(L3 * num_buckets)))
         
         return data
@@ -96,12 +99,24 @@ def read_binary_file(filename, L1, L2, L3, inputs, num_buckets=8):
 def organize_into_buckets(data, L1, L2, L3, num_buckets=8):
     bucketed_data = {
         'l0b': data['l0b'],  # No bucketing for l0b
-        'l0w': data['l0w'],  # No bucketing for l0w
-        'pst': data['pst'],  # Already bucketed as inputs * 8
+        'l0t': [],  
+        'l0p': data['l0p'],  # No bucketing for l0p
+        'pst': [],  # Already bucketed as inputs * 8
         'l1': [],
         'l2': [],
         'l3': []
     }
+    # Clip threat weights
+    for weight_idx in range(79856 * L1):
+        bucketed_data['l0t'].append(max(min(data['l0t'][weight_idx], 127), -128))
+        
+    # Rearrange pst weights
+    threat_start = 22528 * num_buckets
+    threat_end = 102384 * num_buckets
+    psq_start = 0
+    psq_end = 22528 * num_buckets
+    bucketed_data['pst'].extend(data['pst'][threat_start:threat_end])
+    bucketed_data['pst'].extend(data['pst'][psq_start:psq_end])
     
     # Organize l1 layer (bias and weights) into buckets
     for bucket in range(num_buckets):
@@ -151,9 +166,9 @@ def organize_into_buckets(data, L1, L2, L3, num_buckets=8):
     
     return bucketed_data
 
-def convert_binary_format(input_file, output_file, L1=3072, L2=15, L3=32, inputs=22528, num_buckets=8):
+def convert_binary_format(input_file, output_file, L1=128, L2=15, L3=32, num_buckets=8):
     # Read the input binary file
-    data = read_binary_file(input_file, L1, L2, L3, inputs, num_buckets)
+    data = read_binary_file(input_file, L1, L2, L3, num_buckets)
 
     print(f"Read {input_file} successfully.")
     
@@ -179,14 +194,16 @@ def convert_binary_format(input_file, output_file, L1=3072, L2=15, L3=32, inputs
         outfile.write('COMPRESSED_LEB128'.encode('utf-8'))
         write_leb_128_array(outfile, bucketed_data['l0b'])
         
+        outfile.write(struct.pack('<' + 'b' * len(bucketed_data['l0t']), *bucketed_data['l0t']))
+        
         outfile.write('COMPRESSED_LEB128'.encode('utf-8'))
-        write_leb_128_array(outfile, bucketed_data['l0w'])
+        write_leb_128_array(outfile, bucketed_data['l0p'])
         
         outfile.write('COMPRESSED_LEB128'.encode('utf-8'))
         write_leb_128_array(outfile, bucketed_data['pst'])
         
         # Write each bucket of l1, l2 and l3 layers
-        fc_hash_val = 0x6333744A;
+        fc_hash_val = 0x6333712A;
         for bucket in range(num_buckets):
             outfile.write(struct.pack('<I', fc_hash_val))
             
@@ -228,28 +245,27 @@ def main():
     parser = argparse.ArgumentParser(description='Convert binary neural network format')
     parser.add_argument('input_file', type=str, help='Input binary file path')
     parser.add_argument('output_file', type=str, help='Output binary file path')
-    parser.add_argument('--L1', type=int, default=3072, help='L1 parameter (default: 3072)')
+    parser.add_argument('--L1', type=int, default=128, help='L1 parameter (default: 128)')
     parser.add_argument('--L2', type=int, default=15, help='L2 parameter (default: 15)')
     parser.add_argument('--L3', type=int, default=32, help='L3 parameter (default: 32)')
-    parser.add_argument('--inputs', type=int, default=22528, help='inputs parameter (default: 22528)')
     parser.add_argument('--buckets', type=int, default=8, help='Number of buckets (default: 8)')
     
     args = parser.parse_args()
     
-    convert_binary_format(args.input_file, args.output_file, args.L1, args.L2, args.L3, args.inputs, args.buckets)
-    test_foo(args.input_file, args.output_file, args.L1, args.L2, args.L3, args.inputs, args.buckets)
+    convert_binary_format(args.input_file, args.output_file, args.L1, args.L2, args.L3, args.buckets)
+    test_foo(args.input_file, args.output_file)
     print(f"Conversion complete: {args.input_file} -> {args.output_file}")
 
-def test_foo(input_file, output_file, L1=3072, L2=15, L3=32, inputs=22528, num_buckets=8):
+def test_foo(input_file, output_file):
      with open(output_file, 'rb') as f:
         # data = f.read()
 
-        f.seek(69439819)
+        f.seek(6439819)
 
         four_bytes = f.read(4)
 
         integer_value = struct.unpack('<I', four_bytes)[0]
-        print(f"Integer value at position 69389475: {integer_value}")
+        print(f"Integer value at position 6439819: {integer_value}")
 
 
 if __name__ == "__main__":
